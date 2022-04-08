2b2e74df1f7fcc0a4122c938569a543eb837850d
Safak Turgut <safakturgut.dev@gmail.com>
2022-04-08 16:02:49 +0300

    first 10 exercise files created. ex 1-2 solved.

---
+++ ex1.py
@@ -0,0 +1,12 @@
+# Ex 1
+print('Hello World!')
+print('Hello Again')
+print('I like typing this.')
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# Study drills
+print('My another line')
+# print('This line wont print anything because it is a comment')
