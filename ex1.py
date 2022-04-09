2877c0a4aa0b791ea90ba27580e880e766e348f8
Safak Turgut <safakturgut.dev@gmail.com>
2022-04-09 13:15:40 +0300

    ex 3-5 added.

--- ex1.py
+++ ex1.py
@@ -6,7 +6,3 @@ print("This is fun.")
 print('Yay! Printing.')
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
-
-# Study drills
-print('My another line')
-# print('This line wont print anything because it is a comment')
---
+++ ex5_study.py
@@ -0,0 +1,9 @@
+# Ex 5 study
+height = 74  # inches
+weight = 180  # lbs
+
+conv_height = height * 2.54  # cm
+conv_weight = weight * 0.45359237  # kg
+
+print(f'{height} inches', f'{weight} lbs')
+print(f'{conv_height:.2f} cm', f'{conv_weight:.2f} kg')
